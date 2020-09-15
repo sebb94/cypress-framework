6eb8451c80da6f04d15d1c872dfc74f58be3e4cb
sebb94 <omgl0lwth@gmail.com>
2020-09-15 13:16:48 +0200

    login e2e test

---
+++ cypress/integration/e2e/login.spec.js
@@ -0,0 +1,33 @@
+import {
+    url,
+    login_password,
+    login_username
+} from "../../../config"
+import Navbar from '../../page-objects/components/navbar'
+import LoginPage from "../../page-objects/pages/LoginPage"
+
+describe('Login Failed Test', () => {
+    before(() => {
+        cy.visit(url)
+        Navbar.clickSignIn()
+    });
+
+    it('Should login with invalid credentials', () => {
+        LoginPage.login('invalid_username','invalid_password')
+    });
+
+    it('Should display error message', () => {
+        LoginPage.displayErrorMessage()
+    });
+});
+
+describe('Login Success Test', () => {
+    before(() => {
+        cy.visit(url)
+        Navbar.clickSignIn()
+    });
+
+    it('Should login into application', () => {
+        LoginPage.login(login_username,login_password)
+    });
+});
